d93c3ad34c8155d79acc98e42a63597c6aa5b304
mikkimat81539 <mmatthews81539@gmail.com>
2026-08-01 17:51:54 -0500

    added dot hand connection

--- cameraObject.py
+++ cameraObject.py
@@ -12,6 +12,10 @@ from numpy import *
 
 class Camera_Detection:
 	def Camera(surface):
+		# TIME
+		start_time = time.time()
+		
+
 		# DEFINE TASKS
 		BaseOptions = mp.tasks.BaseOptions
 		HandLandmarker = mp.tasks.vision.HandLandmarker
@@ -30,6 +34,8 @@ class Camera_Detection:
 			min_tracking_confidence = 0.5,
 			num_hands = 2
 		)	
+		
+		landmarker = HandLandmarker.create_from_options(options)
 	
 
 		# LOAD CAMERA FROM OPENCV	
@@ -44,6 +50,9 @@ class Camera_Detection:
 		if not ret:
 			running = False
 
+		timestamp_ms = int((time.time() - start_time) * 1000)
+
+		# CAMERA WINDOW
 		frame = np.fliplr(frame) # flip camera
 		frame = np.rot90(frame)	# rotate camera
 	
@@ -51,10 +60,25 @@ class Camera_Detection:
 
 		frame = cv.resize(frame, (210, 320)) # resize camera screen
 
+		# Convert the frame received from OpenCV to a MediaPipe’s Image object.
+		mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
+
+		# HAND CONNECTORS
+		result = landmarker.detect_for_video(mp_image, timestamp_ms) # hand_landmarker_result
+
+		height, width, throwaway = frame.shape # get the actual frame dimensions
+
+		for i in result.hand_landmarks:
+			for j in i:
+				x = int(j.x*width)
+				y = int(j.y*height)
+
+				cv.circle(frame,(x, y), 2, (0,255,0), -1)
+
+		# TURN ARRAY INTO PYGAME SURFACE
 		camArray = pygame.surfarray.make_surface(frame) # Copy an array to a new surface
 
-
-		surface.blit(camArray, (0, 0))
+		surface.blit(camArray, (0, 0)) # BLIT ONTO PYGAME SURFACE
 
 		cap.release()
 		cv.destroyAllWindows()
